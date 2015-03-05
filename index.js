687f46f17cb624ba7d7b3fa08c9df3162c8cf647
Lowell Bander <lowellbander@gmail.com>
2015-03-04 17:53:04 -0800

    hopefully works

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/echo', function(request, response) {
+  response.send('text: ' + request.query.text +
+        ' and url is: ' + process.env.TEST_WEBHOOK);
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
